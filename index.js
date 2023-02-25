f1398d18e9c1a7eb4e3341c6b2dc79c399bf8f4e
Sagar1079 <sagarwankhade896@gmail.com>
2023-02-25 14:22:44 +0530

    C

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const { connection } = require("./config/db");
 
 const app = express();
-app.use(cors())
+app.use(cors({origin:"*"}))
 app.use(express.json())
 
 app.use('/users',userRouter)
